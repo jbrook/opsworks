f7851cce3c9d8b09c31583f9f5170f2edda2aa1b
Eric Ruvio <eruvio90@gmail.com>
2013-09-14 11:44:15 -0400

    no message

--- deploy/recipes/default.rb
+++ deploy/recipes/default.rb
@@ -1,9 +1,35 @@
-include_recipe 'dependencies'
+node[:deploy].each do |app_name, deploy|
 
-node[:deploy].each do |application, deploy|
+  # create proper folders if not exist
+  directory "#{deploy[:deploy_to]}/current/core/cache" do
+    group deploy[:group]
+    owner "apache"
+    mode 00755
+    action :create
+    only_if do
+      File.directory?("#{deploy[:deploy_to]}/current")
+    end
+  end
 
-  opsworks_deploy_user do
-    deploy_data deploy
+  directory "#{deploy[:deploy_to]}/current/core/cache/zend" do
+    group deploy[:group]
+    owner "apache"
+    mode 00755
+    action :create
+    only_if do
+      File.directory?("#{deploy[:deploy_to]}/current")
+    end
+  end
+
+  # run composer installer without dev dependencies
+  script "install_composer" do
+    interpreter "bash"
+    user "root"
+    cwd "#{deploy[:deploy_to]}/current"
+    code <<-EOH
+    curl -sS https://getcomposer.org/installer | php
+    php composer.phar install --no-dev
+    EOH
   end
 
 end
