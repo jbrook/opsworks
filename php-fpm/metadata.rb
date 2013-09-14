a4348b9ccbc63c164dad2e33f9c1ee103b0aa43b
Eric Ruvio <eruvio90@gmail.com>
2013-09-14 15:51:36 -0400

    no message

--- php-fpm/metadata.rb
+++ php-fpm/metadata.rb
@@ -6,7 +6,6 @@ description      "Installs/Configures php-fpm"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.4.6"
 
-depends "yum"
 
 %w{ debian ubuntu centos redhat fedora amazon }.each do |os|
   supports os
