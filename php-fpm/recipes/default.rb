59011aaf0dd4bdf94e207cf0cf55374ead168b77
Eric Ruvio <eruvio90@gmail.com>
2013-09-14 16:22:36 -0400

    no message

--- php-fpm/recipes/default.rb
+++ php-fpm/recipes/default.rb
@@ -1,24 +1,3 @@
 package 'php-fpm' do
   action :install
 end
-
-template node['php-fpm']['conf_file'] do
-  source "php-fpm.conf.erb"
-  mode 00644
-  owner "root"
-  group "root"
-end
-
-php_fpm_service_name = "php-fpm"
-
-node['php-fpm']['pools'].each do |pool|
-  fpm_pool pool do
-    php_fpm_service_name php_fpm_service_name
-  end
-end
-
-service "php-fpm" do
-  service_name php_fpm_service_name
-  supports :start => true, :stop => true, :restart => true, :reload => true
-  action [ :enable, :restart ]
-end
