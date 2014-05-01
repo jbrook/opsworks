77a5a8d13ee61a23a32dfa9889bbdc23422510e1
jbrook <james.brook@spaaza.com>
2014-05-01 14:27:30 +0200

    Update install.rb

--- php-fpm/recipes/install.rb
+++ php-fpm/recipes/install.rb
@@ -9,13 +9,15 @@ template node['php-fpm']['conf_file'] do
   group "root"
 end
 
+php_fpm_service_name = 'php-fpm'
+
 node['php-fpm']['pools'].each do |pool|
   fpm_pool pool do
     php_fpm_service_name php_fpm_service_name
   end
 end
 
-service "php-fpm" do
+service php_fpm_service_name do
   service_name php_fpm_service_name
   supports :start => true, :stop => true, :restart => true, :reload => true
   action [ :enable, :restart ]
